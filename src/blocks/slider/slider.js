9d1c1331afdcb45e07269bec9031fc835eb3f5b8
Дмитрий Васильев <vasilev8dmitriy@gmail.com>
2019-02-20 17:44:00 +0300

    добавлен скрипт перключения слайдов по ссылкам меню

--- src/blocks/slider/slider.js
+++ src/blocks/slider/slider.js
@@ -1,3 +1,7 @@
+$('.js-slider').children().each(function(index) {
+  $(this).attr('data-hash', index);
+});
+
 $('.js-slider').addClass('owl-carousel')
   .owlCarousel({
     items: 1,
@@ -6,5 +10,10 @@ $('.js-slider').addClass('owl-carousel')
     autoHeight: true,
     navContainer: '.slider__wrapper',
     navClass: ['slider__btn slider__btn--prev', 'slider__btn slider__btn--next'],
-    navText: ['<span class="visually-hidden">Предыдущий</span>', '<span class="visually-hidden">Следующий</span>']
+    navText: ['<span class="visually-hidden">Предыдущий</span>', '<span class="visually-hidden">Следующий</span>'],
+    URLhashListener: true
+  });
+
+$('.js-menu-link').each(function(index) {
+  $(this).attr('href', '#' + index);
 });
